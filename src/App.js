3ac28b655a696e16926cfba41a6ba36a5039d3f7
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-18 23:06:30 -0300

    config redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Router from "./router";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+      </BrowserRouter>
+    </Provider>
   );
 }
 

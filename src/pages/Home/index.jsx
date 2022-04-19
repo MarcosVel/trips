a2807b22125d2a0068c55dd374afaf00a4b3b397
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-18 22:17:09 -0300

    styled home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
+import { MdFlightTakeoff } from "react-icons/md";
+import "./style.css";
 
 function Home() {
   const [trips, setTrips] = useState([]);
@@ -14,7 +16,22 @@ function Home() {
   }, []);
 
   return (
-    <div>Home</div>
+    <div>
+      <div className="box">
+        {trips.map(trips => (
+          <li key={trips.id}>
+            <img src={trips.image} alt={trips.title} />
+            <strong>{trips.title}</strong>
+            <span>Status: {trips.status ? "Disponível" : "Indisponível"}</span>
+
+            <button onClick={() => {}}>
+              <MdFlightTakeoff size={16} color="#fff" />
+              <span>Solicitar reserva</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
   );
 }
 

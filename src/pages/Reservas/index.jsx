31dcd623167866756791e97eb2c52b31eb441e17
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-18 22:31:20 -0300

    styled reservas

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
+import { MdDelete } from "react-icons/md";
 
 function Reservas() {
   return (
-    <div>Reservas</div>
-  )
+    <div>
+      <h1 className="title">Você solicitou 1 reserva</h1>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="blahs"
+        />
+        <strong>Viagem</strong>
+        <span>Quantidade: 2</span>
+        <button onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button>Solicitar reservas</button>
+      </footer>
+    </div>
+  );
 }
 
-export default Reservas
+export default Reservas;

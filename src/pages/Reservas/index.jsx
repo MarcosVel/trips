a435918239c6687df5b0fb7a8eecdf515deb9752
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-21 22:02:32 -0300

    adding reservas

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import "./style.css";
 import { MdDelete } from "react-icons/md";
+import { useSelector } from "react-redux";
 
 function Reservas() {
+  const reserves = useSelector(state => state.reserve);
+
   return (
     <div>
-      <h1 className="title">Você solicitou 1 reserva</h1>
-      <div className="reservas">
-        <img
-          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-          alt="blahs"
-        />
-        <strong>Viagem</strong>
-        <span>Quantidade: 2</span>
-        <button onClick={() => {}}>
-          <MdDelete size={20} color="#191919" />
-        </button>
-      </div>
+      <h1 className="title">Você solicitou {reserves.length} reserva(s)</h1>
+
+      {reserves.map(reserve => (
+        <div className="reservas" key={reserve.id}>
+          <img src={reserve.image} alt={reserve.title} />
+          <strong>{reserve.title}</strong>
+          <span>Quantidade: 2</span>
+          <button onClick={() => {}}>
+            <MdDelete size={20} color="#191919" />
+          </button>
+        </div>
+      ))}
 
       <footer>
         <button>Solicitar reservas</button>

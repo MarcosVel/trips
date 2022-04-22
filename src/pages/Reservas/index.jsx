370f7a12f4d7117b1d1c8710c587d985b5893eb2
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-21 22:10:06 -0300

    qtd of each trip in reserva

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -14,7 +14,7 @@ function Reservas() {
         <div className="reservas" key={reserve.id}>
           <img src={reserve.image} alt={reserve.title} />
           <strong>{reserve.title}</strong>
-          <span>Quantidade: 2</span>
+          <span>Quantidade: {reserve.amount}</span>
           <button onClick={() => {}}>
             <MdDelete size={20} color="#191919" />
           </button>
--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -1,7 +1,13 @@
 export default function reserve(state = [], action) {
   switch (action.type) {
     case "ADD_RESERVE":
-      return [...state, action.trip];
+      return [
+        ...state,
+        {
+          ...action.trip,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }

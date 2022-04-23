9cc6b6654509ab5e59429d3b383334e05fabe071
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-23 20:47:19 -0300

    refactoring redux

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,13 +1,26 @@
-import { call, put, all, takeLatest } from "redux-saga/effects";
+import { select, call, put, all, takeLatest } from "redux-saga/effects";
 import api from "../../../services/api";
-import { addReserveSuccess } from "./action";
+import { addReserveSuccess, updateAmountReserve } from "./action";
 
 function* addToReserve({ id }) {
-  const response = yield call(api.get, `trips/${id}`);
+  const tripExist = yield select(state =>
+    state.reserve.find(trip => trip.id === id)
+  );
 
-  yield put(addReserveSuccess(response.data));
+  if (tripExist) {
+    const amount = tripExist.amount + 1;
+
+    yield put(updateAmountReserve(id, amount));
+  } else {
+    const response = yield call(api.get, `trips/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+    };
+
+    yield put(addReserveSuccess(data));
+  }
 }
 
-export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
-])
+export default all([takeLatest("ADD_RESERVE_REQUEST", addToReserve)]);

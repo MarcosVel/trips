f5d23584e50e1df9913a8ebed59bc2097be787ef
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-24 17:16:19 -0300

    adjust qtd with sagas

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,6 +1,6 @@
-import { select, call, put, all, takeLatest } from "redux-saga/effects";
+import { all, call, put, select, takeLatest } from "redux-saga/effects";
 import api from "../../../services/api";
-import { addReserveSuccess, updateAmountReserve } from "./action";
+import { addReserveSuccess, updateAmountSuccess } from "./action";
 
 function* addToReserve({ id }) {
   const tripExist = yield select(state =>
@@ -15,13 +15,13 @@ function* addToReserve({ id }) {
 
   const amount = stockReserved + 1;
 
-  if(amount > stockAmount) {
-    alert('Estoque de viagem esgotado')
+  if (amount > stockAmount) {
+    alert("Estoque de viagem esgotado");
     return;
   }
 
   if (tripExist) {
-    yield put(updateAmountReserve(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `trips/${id}`);
 
@@ -34,4 +34,22 @@ function* addToReserve({ id }) {
   }
 }
 
-export default all([takeLatest("ADD_RESERVE_REQUEST", addToReserve)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    alert("Estoque de viagem esgotado");
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest("ADD_RESERVE_REQUEST", addToReserve),
+  takeLatest("UPDATE_AMOUNT_REQUEST", updateAmount),
+]);
